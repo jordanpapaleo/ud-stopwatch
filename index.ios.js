1e44a13b554c99edf6f039b1d9cdccd450766994
Jordan Papaleo <papaleowebdev@gmail.com>
2016-04-05 14:33:23 -0700

    start button working

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,36 @@
 import React, {
   AppRegistry,
+  TouchableHighlight,
   Component,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+
+import formatTime from 'minutes-seconds-milliseconds'
+import Button from './src/Button.js'
 
 class stopwatch extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      timeElapsed: null
+    }
+
+    this.handleStartPress = this.handleStartPress.bind(this)
+  }
+
+  handleStartPress (e) {
+    const startTime = new Date()
+
+    setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - startTime
+      })
+    }, 30)
+  }
+
   render() {
     const styles = {
       container: {
@@ -16,8 +40,15 @@ class stopwatch extends Component {
       header: [
         {
           flex: 1
-        },
-        this.styleBorder('yellow')
+        }
+      ],
+      buttonWrapper: [
+        {
+          flex: 3, // 3/8th
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center'
+        }
       ]
     }
 
@@ -25,68 +56,42 @@ class stopwatch extends Component {
       <View style={styles.container}>
         <View style={styles.header}>
           {this.renderTimer()}
-          <View style={this.styleBorder('green')}>
-            {this.renderStartButton()}
-            {this.renderLapButton()}
+          <View style={styles.buttonWrapper}>
+            <Button label='Start' handler={this.handleStartPress} style={{ backgroundColor: 'green' }}/>
+            <Button label='Lap' handler={() => { console.log('Lap pressed') }} />
           </View>
         </View>
+
         {this.renderFooter()}
       </View>
     )
   }
 
   renderTimer () {
-    const styles = [
-      this.styleBorder('red')
-    ]
-
-    return (
-      <View style={styles}>
-        <Text>00:00.0</Text>
-      </View>
-    )
-  }
-
-  renderStartButton () {
+    const { timeElapsed } = this.state
     const styles = {
-
+      flex: 5, // 5/8th
+      justifyContent: 'center',
+      alignItems: 'center'
     }
 
     return (
-      <View>
-        <Text>Start</Text>
-      </View>
-    )
-  }
-
-  renderLapButton () {
-    return (
-      <View>
-        <Text>Lap</Text>
+      <View style={styles}>
+        <Text style={{fontFamily: 'Cochin', fontSize: 60}}>{timeElapsed ? formatTime(timeElapsed) : '00:00.00' }</Text>
       </View>
     )
   }
 
   renderFooter () {
-    const styles = [
-      {
-        flex: 1
-      },
-      this.styleBorder('blue')
-    ]
+    const styles = {
+      flex: 1
+    }
 
     return (
       <View style={styles}>
         <Text>List of Laps</Text>
       </View>
     )
-  }
-
-  styleBorder (color) {
-    return {
-      borderColor: color,
-      borderWidth: 4
-    }
   }
 }
 
